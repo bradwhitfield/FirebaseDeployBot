1cc1955160039c0006bc78d9808d5013d1236042
Brad Whitfield <bradswhitfield@gmail.com>
2017-12-09 11:01:36 -0500

    Started work on Cloud Functions.

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
+
+func F(w http.ResponseWriter, r *http.Request) {
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriterHeader(500)
+		log.Panic(err)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(response{Message: string(d)}); err != nil {
+		w.WriterHeader(500)
+		return
+	}
+}
