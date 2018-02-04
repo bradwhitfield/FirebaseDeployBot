fa31110547f1d7a745178ab29078e4e62ae80cce
Brad Whitfield <bradswhitfield@gmail.com>
2018-02-04 17:26:17 -0500

    Initial working version.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # TODO
 
 * Cleanup old Firebase deployments
-* Cleanup GS bucket
+
+# Testing Locally
+
+```bash
+# Test go code locally
+go run main.go
+# Test Cloud Functions locally
+docker run -it -v ${PWD}:/go/src/github.com/bradwh
+itfield/FirebaseDeployBot/ --workdir /go/src/github.com/bradwhitfield/FirebaseDeployBot/ golang:stretch bash
+go get golang.org/x/net/context
+go get golang.org/x/oauth2/google
+go get google.golang.org/api/cloudbuild/v1
+cat request.json | cloud-functions-go-shim -entry-point F -event-type http -plugin-path functions.so
+```
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,43 +16,75 @@ type response struct {
 	Message string `json:"message"`
 }
 
+type BuildRequest struct {
+	ProjectID string `json:"project-id"`
+	Token     string `json:"token"`
+	GitURL    string `json:"git-url"`
+}
+
 func F(w http.ResponseWriter, r *http.Request) {
+	var br BuildRequest
+	log.Println("Starting POST call.")
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(500)
 		log.Panic(err)
 		return
 	}
+	err = json.Unmarshal(d, &br)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Panic(err)
+	}
+	build(br)
 
-	if err := json.NewEncoder(w).Encode(response{Message: string(d)}); err != nil {
+	if err := json.NewEncoder(w).Encode(response{Message: "Build Started."}); err != nil {
 		w.WriteHeader(500)
 		return
 	}
 }
 
 func main() {
-	id := "nimble-card-172800"
-	build(id)
+	test, err := ioutil.ReadFile("main.json")
+	if err != nil {
+		log.Panic(err)
+	}
+	var br BuildRequest
+	err = json.Unmarshal(test, &br)
+	if err != nil {
+		log.Panic(err)
+	}
+	build(br)
 }
 
-func build(id string) {
+func build(br BuildRequest) {
 	// Google Default Auth
+	log.Println("Starting build function.")
 	ctx := context.TODO()
 	client, err := google.DefaultClient(ctx, cloudbuild.CloudPlatformScope)
 	if err != nil {
 		log.Print(err)
 		log.Panic("Error retrieving default credentials.")
 	}
-	log.Print(client)
 	svc, err := cloudbuild.New(client)
 	if err != nil {
 		log.Print(err)
 		log.Panic("Unable to initialize GCP Container client.")
 	}
 
+	token := fmt.Sprintf("%s%s", "FIREBASE_DEPLOY_TOKEN=", br.Token)
+	project := fmt.Sprintf("%s%s", "FIREBASE_PROJECT=", br.ProjectID)
+	git := fmt.Sprintf("%s%s", "GIT_REPO=", br.GitURL)
+
+	log.Println("Creating build steps.")
 	steps := []*cloudbuild.BuildStep{
 		&cloudbuild.BuildStep{
-			Name: "hello-world",
+			Name: "bradwhitfield/firebasedeploybot",
+			Env: []string{
+				token,
+				project,
+				git,
+			},
 		},
 	}
 
@@ -59,12 +92,16 @@ func build(id string) {
 		Steps: steps,
 	}
 
-	create := svc.Projects.Builds.Create(id, build)
+	log.Println("Creating build and starting it.")
+	create := svc.Projects.Builds.Create(br.ProjectID, build)
 	results, err := create.Do()
 	if err != nil {
 		log.Panic(err)
 	}
-	log.Print(results.HTTPStatusCode)
 	rsp, _ := results.MarshalJSON()
-	log.Print(string(rsp))
+	log.Printf(
+		"Build execution request triggered with HTTP status code of %d and response of %s.\n",
+		results.HTTPStatusCode,
+		string(rsp),
+	)
 }
